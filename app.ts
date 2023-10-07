9927dc38826d6f1c16161e459a7bf915b1d444e0
Pratham <chudasamapratham142@gmail.com>
2023-10-07 14:38:49 +0530

    try this

--- app.ts
+++ app.ts
@@ -7,10 +7,13 @@ import authRoute from "./routes/auth";
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const morgan = require("morgan");
 
 
 app.use(cors());
 app.use(express.json());
+
+app.use(morgan("dev"));
 
 app.get("/", (req: Request, res: Response) => {
 	ResponseHandler.success({
