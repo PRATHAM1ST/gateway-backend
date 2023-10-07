29e78cf5817075c4e9609aed1dd01a097d0c631d
kirtan <kirtan1902@gmail.com>
2023-10-07 15:43:38 +0530

    otp and approval mail template

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
-import { Request, Response } from "express";
-import ResponseHandler from "./handler/ResponseHandler";
-
 const express = require("express");
 const cors = require("cors");
 const app = express();
 
 app.use(cors());
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/", (req, res) => {
 	ResponseHandler.success({
 		req,
 		res,
@@ -16,4 +13,7 @@ app.get("/", (req: Request, res: Response) => {
 	});
 });
 
+app.listen(4000, () => {
+	console.log("Example app listening on port 4000!");
+});
 export default app;
--- index.ts
+++
@@ -1,5 +0,0 @@
-import app from "./app";
-
-app.listen(4000, () => {
-	console.log("Example app listening on port 4000!");
-});
